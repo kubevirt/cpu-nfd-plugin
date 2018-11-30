ad3019974030a43f08f84b48c1002809c6762525
Karel Šimon <ksimon@redhat.com>
2018-11-30 09:58:42 +0100

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # NFD-Host-supported-cpus
-**NFD-Host-supported-cpus**
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/ksimon1/nfd-host-supported-cpus)](https://goreportcard.com/report/github.com/ksimon1/nfd-host-supported-cpus)
+
+**NFD-Host-supported-cpus** is plugin for [node-feature-discovery](https://github.com/kubernetes-sigs/node-feature-discovery). It creates list of all supported cpu models on host, which NFD then exposes as node labels.
+
+**Usage:**
+```
+kubectl create -f nfd-host-supported-cpus.yaml.template
+
+kubectl describe nodes
+```
--- pkg/collector/collector_test.go
+++ pkg/collector/collector_test.go
@@ -1,9 +1,9 @@
 package collector
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
-  "io/ioutil"
-  "os"
 )
 
 var mockData = `<domainCapabilities>
@@ -25,73 +25,73 @@ var mockData = `<domainCapabilities>
 </domainCapabilities>`
 
 func writeMockDataFile(path, data string) error {
-  err := ioutil.WriteFile(path, []byte(data), 0644)
-  if err != nil {
-    return err
-  }
-  return nil
+	err := ioutil.WriteFile(path, []byte(data), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func deleteMockFile(path string) error{
-  err := os.Remove(path)
-  if err != nil {
-    return err
-  }
-  return nil
+func deleteMockFile(path string) error {
+	err := os.Remove(path)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // TestCollectData test CollectData function
 func TestCollectData(t *testing.T) {
-  filePath := "/tmp/virsh-domcapabilities.xml"
-  err := writeMockDataFile(filePath, mockData)
-  if err != nil {
-    t.Error("writeMockDataFile should not throw error: " + err.Error())
-    t.FailNow()
-  }
+	filePath := "/tmp/virsh-domcapabilities.xml"
+	err := writeMockDataFile(filePath, mockData)
+	if err != nil {
+		t.Error("writeMockDataFile should not throw error: " + err.Error())
+		t.FailNow()
+	}
 
-  result, err := CollectData(filePath)
-  if err != nil {
-    t.Error("CollectData should not throw error: " + err.Error())
-  }
+	result, err := CollectData(filePath)
+	if err != nil {
+		t.Error("CollectData should not throw error: " + err.Error())
+	}
 
-  if len(result) != 1 {
-    t.Error("CollectData should return one cpu model")
-  }
+	if len(result) != 1 {
+		t.Error("CollectData should return one cpu model")
+	}
 
-  if len(result) == 1 {
-    if result[0] != "haswell" {
-      t.Error("cpu model should equal to haswell")
-    }
+	if len(result) == 1 {
+		if result[0] != "haswell" {
+			t.Error("cpu model should equal to haswell")
+		}
 
-    if result[0] == "Haswell" {
-      t.Error("cpu model should contain lower cased chars")
-    }
-  }
+		if result[0] == "Haswell" {
+			t.Error("cpu model should contain lower cased chars")
+		}
+	}
 
-  err = deleteMockFile(filePath)
-  if err != nil {
-    t.Error("deleteMockFile should not throw error: " + err.Error())
-  }
+	err = deleteMockFile(filePath)
+	if err != nil {
+		t.Error("deleteMockFile should not throw error: " + err.Error())
+	}
 
-  result, err = CollectData("")
-  if err == nil {
-    t.Error("CollectData should throw error, because of empty path")
-  }
+	result, err = CollectData("")
+	if err == nil {
+		t.Error("CollectData should throw error, because of empty path")
+	}
 
-  if result != nil {
-    t.Error("CollectData should return nil")
-  }
+	if result != nil {
+		t.Error("CollectData should return nil")
+	}
 
-  err = writeMockDataFile(filePath, "pat a mat")
-  if err != nil {
-    t.Error("writeMockDataFile should not throw error: " + err.Error())
-    t.FailNow()
-  }
+	err = writeMockDataFile(filePath, "pat a mat")
+	if err != nil {
+		t.Error("writeMockDataFile should not throw error: " + err.Error())
+		t.FailNow()
+	}
 
-  _, err = CollectData(filePath)
-  if err == nil {
-    t.Error("CollectData should throw error, because data in file are not in xml")
-  }
+	_, err = CollectData(filePath)
+	if err == nil {
+		t.Error("CollectData should throw error, because data in file are not in xml")
+	}
 
-  deleteMockFile(filePath)
+	deleteMockFile(filePath)
 }
